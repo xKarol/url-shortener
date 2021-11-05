8fe9c1bdda1a14b4af1894233fe1ef631a87374d
xKarol <karolpiskorz8@gmail.com>
2021-11-05 16:26:09 +0100

    ADD: header

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { ReactComponent as Logo } from "../images/logo.svg";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="header__left-side">
+          <Logo className="header__logo" />
+          <nav className="header__links">
+            <a href="/">Features</a>
+            <a href="/">Pircing</a>
+            <a href="/">Resources</a>
+          </nav>
+        </div>
+        <div className="header__right-side">
+          <button className="header__button sign-in">Login</button>
+          <button className="header__button">Sign Up</button>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
