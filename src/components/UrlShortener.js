7ac9b0eced345fd0d2d50517df1a7aae428866dd
xKarol <karolpiskorz8@gmail.com>
2021-11-07 13:50:42 +0100

    ADD: copy link, obsluga bledow

--- src/components/UrlShortener.js
+++ src/components/UrlShortener.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import Loading from "./Loading.js";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import copy from "copy-to-clipboard";
+
 const Shortener = () => {
   const [url, setUrl] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const [links, setLinks] = useState([]);
-
-  // const copyLink = (id) => {
-  //   console.log(id);
-  //   CopyToClipboard("test");
-  // };
 
   const checkForm = () => {
     if (url.length < 3) {
@@ -31,10 +27,24 @@ const Shortener = () => {
       const shortLink = data.result.full_short_link;
       setLinks([...links, { main_link: url, short_link: shortLink }]);
     } catch {
-      if (data.error_code === 2) {
-        setError("Wprowadzony link jest nieprawidłowy.");
-      } else {
+      const code = data.error_code;
+      if (code < 0 || code > 10) {
         setError("Wystąpił niespodziewany problem...");
+      } else {
+        const errors = [
+          "Wystąpił niespodziewany problem...", // code_0
+          "Nie wprowadziłeś żadnego linku", // code_1
+          "Wprowadzono nieprawidłowy adres URL", // code_2
+          "Odczekaj chwilę", // code_3
+          "Adres IP został zablokowany z powodu naruszenia warunków korzystania z usługi", // code_4
+          "Wygenerowany kod jest już zajęty. Spróbuj ponownie...", // code_5
+          "Wystąpił niespodziewany problem...", // code_6
+          "Nie określono kodu (parametr „code” jest pusty)", // code_7
+          "Wprowadzono nieprawidłowy adres URL", // code_8
+          "Brak wymaganych parametrów", // code_9
+          "Tego linku nie można skrócić", // code_10
+        ];
+        setError(errors[code]);
       }
     } finally {
       setLoading(false);
@@ -46,7 +56,7 @@ const Shortener = () => {
         <input
           type="text"
           placeholder="Shorten a link here..."
-          className="shortener__input"
+          className={error ? "shortener__input --error" : "shortener__input"}
           value={url}
           onChange={(e) => setUrl(e.target.value)}
         />
@@ -58,7 +68,11 @@ const Shortener = () => {
       {links && (
         <ul className="shortener__links">
           {links.map((data, index) => (
-            <Link {...data} key={index} />
+            <Link
+              {...data}
+              key={index}
+              copy={(index) => copy(data.short_link)}
+            />
           ))}
         </ul>
       )}
@@ -68,7 +82,15 @@ const Shortener = () => {
 
 export default Shortener;
 
-const Link = ({ main_link, short_link }) => {
+const Link = ({ main_link, short_link, copy }) => {
+  const [copied, setCopied] = useState(false);
+  const copyLink = () => {
+    copy();
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
   return (
     <>
       <li className="shortener__link-box">
@@ -77,8 +99,15 @@ const Link = ({ main_link, short_link }) => {
           <a className="shortener__link__shorten" href={short_link}>
             {short_link}
           </a>
-          <button className="shortener__button --list">
-            Copy
+          <button
+            className={
+              copied
+                ? "shortener__button --list --copied"
+                : "shortener__button --list"
+            }
+            onClick={copied ? null : copyLink}
+          >
+            {copied ? "Copied!" : "Copy"}
           </button>
         </div>
       </li>
