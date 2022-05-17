226df0367fa49a3c6d25cfae050fdde919c74c2f
xKarol <karolpiskorz8@gmail.com>
2022-05-17 10:47:19 +0200

    Centered footer nav links on small breakpoint

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,22 +7,24 @@ export default function Footer() {
   return (
     <footer className="footer">
       <Logo fill="#fff" className="footer__logo" />
-      <Nav title={"Features"}>
-        <NavLink name={"Link Shortening"} />
-        <NavLink name={"Branded Links"} />
-        <NavLink name={"Analytics"} />
-      </Nav>
-      <Nav title={"Resources"}>
-        <NavLink name={"Blog"} />
-        <NavLink name={"Developers"} />
-        <NavLink name={"Support"} />
-      </Nav>
-      <Nav title={"Company"}>
-        <NavLink name={"About"} />
-        <NavLink name={"Our Team"} />
-        <NavLink name={"Careers"} />
-        <NavLink name={"Contact"} />
-      </Nav>
+      <section className="footer__nav-container">
+        <Nav title={"Features"}>
+          <NavLink name={"Link Shortening"} />
+          <NavLink name={"Branded Links"} />
+          <NavLink name={"Analytics"} />
+        </Nav>
+        <Nav title={"Resources"}>
+          <NavLink name={"Blog"} />
+          <NavLink name={"Developers"} />
+          <NavLink name={"Support"} />
+        </Nav>
+        <Nav title={"Company"}>
+          <NavLink name={"About"} />
+          <NavLink name={"Our Team"} />
+          <NavLink name={"Careers"} />
+          <NavLink name={"Contact"} />
+        </Nav>
+      </section>
 
       <Socials />
       <span>
